4b74faf17da82e0289489ae81f584f2f65fb02bc
Robert Knauer <robert@capsaicin-dev.de>
2019-09-19 10:31:20 +0200

    implemeted stepper control over serial port

--- arduino-software/Config.hh
+++ arduino-software/Config.hh
@@ -1,5 +1,6 @@
 #pragma once
 
+const long serialBaudrate = 86400;
 const uint8_t pinStep = 4;
 const uint8_t pinDirection = 5;
 const uint8_t pinEnable = 6;
@@ -10,3 +11,4 @@ const float acceleration = 1000;
 const long positionMin = 400;
 const long positionMax = 29000;
 const uint8_t positionsNum = 5;
+const uint8_t commandLengthMax = 20;
--- arduino-software/arduino-software.ino
+++ arduino-software/arduino-software.ino
@@ -2,11 +2,14 @@
 #include "Config.hh"
 
 const long stepsPerPosition = (positionMax - positionMin) / (positionsNum - 1);
-uint8_t positionIdxCurrent = 0;
+char command[commandLengthMax+1];
+uint8_t commandLength = 0;
+bool positionReached = true;
+bool endstopErrorPrinted = false;
 
 AccelStepper stepper(AccelStepper::DRIVER, pinStep, pinDirection);
 
-void home() {
+void home(bool melody=false) {
   /* Run backwards until the endstop switch is pressed: */
   stepper.setSpeed(-homeStepsPerSecond);
   while (digitalRead(pinEndstop) == HIGH) {
@@ -16,19 +19,22 @@ void home() {
   /* Zero the position: */
   stepper.setCurrentPosition(0);
   /* Play a short melody to signal that we're ready: */
-  tone(pinStep, 8372); // C
-  delay(500);
-  tone(pinStep, 10548); // E
-  delay(500);
-  tone(pinStep, 12544); // G
-  delay(500);
-  noTone(pinStep);
-  delay(1000);
+  if (melody) {
+    tone(pinStep, 8372); // C
+    delay(500);
+    tone(pinStep, 10548); // E
+    delay(500);
+    tone(pinStep, 12544); // G
+    delay(500);
+    noTone(pinStep);
+  }
   /* Go to minimal position: */
   stepper.runToNewPosition(positionMin);
 }
 
 void setup() {
+  /* Setup serial port: */
+  Serial.begin(serialBaudrate);
   /* Setup stepper motor: */
   stepper.setEnablePin(pinEnable);
   stepper.setPinsInverted(false, false, true);
@@ -38,23 +44,68 @@ void setup() {
   /* Setup endstop switch: */
   pinMode(pinEndstop, INPUT);
   /* Go to home position: */
-  home();
+  home(); /* TODO: Enable melody */
+  /* Report that we're ready: */
+  Serial.print(F("INF READY POSNUM "));
+  Serial.println(positionsNum);
+}
+
+void parseCommand(char *command) {
+  if (strcmp(command, "HOME") == 0) {
+    /* Go to home position. */
+    Serial.println(F("CMD HOME BLOCKING"));
+    home();
+    Serial.println(F("INF HOMED"));
+  } else if (strstr(command, "GOTO ") == command) {
+    /* Go to given position. */
+    int position = atoi(strchr(command, ' ') + 1);
+    if (position < 0 || position >= positionsNum) {
+      Serial.print(F("ERR INVALIDPOS "));
+      Serial.println(position);
+    } else {
+      Serial.print(F("CMD GOINGTO NONBLOCKING "));
+      Serial.println(position);
+      stepper.moveTo(positionMin + position * stepsPerPosition);
+      positionReached = false;
+    }
+  } else {
+    Serial.print(F("ERR UNKNOWNCMD "));
+    Serial.println(command);
+  }
 }
 
 void loop() {
   /* If endstop switch is pressed, stop instantly: */
   if (digitalRead(pinEndstop) == LOW) {
     stepper.stop();
-    return;
-    /* TODO: Enter some kind of emergency mode? */
-  }
-  /* Move to the next position if we arrived at the current one: */
-  if (stepper.distanceToGo() == 0) {
-    positionIdxCurrent++;
-    if (positionIdxCurrent == positionsNum) {
-      positionIdxCurrent = 0;
+    if (!endstopErrorPrinted) {
+      Serial.println(F("ERR ENDSTOPUNEXPECTED"));
+      endstopErrorPrinted = true;
     }
-    stepper.moveTo(positionMin + positionIdxCurrent * stepsPerPosition);
+    /* Suppress "Position reached." message: */
+    positionReached = true;
+    return;
+  } else {
+    endstopErrorPrinted = false;
+  }
+  /* Check for new commands: */
+  while (Serial.available() > 0) {
+    char chr = Serial.read();
+    if (chr != '\n' && chr != '\r' && commandLength < commandLengthMax) {
+      /* Save received char in our buffer: */
+      command[commandLength++] = chr;
+    } else {
+      /* Command finished, add null byte and prepare for next one: */
+      command[commandLength] = '\0';
+      commandLength = 0;
+      /* ... and parse it: */
+      parseCommand(command);
+    }
+  }
+  /* Print a message if we arrived at our target: */
+  if (!positionReached && stepper.distanceToGo() == 0) {
+    Serial.println(F("INF POSREACHED"));
+    positionReached = true;
   }
   /* Run the motor: */
   stepper.run();
