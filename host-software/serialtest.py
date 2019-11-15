fc87bdd29d3c6d36b159c31b2cc6f4d8236b7749
Robert Knauer <robert@capsaicin-dev.de>
2019-11-15 21:19:56 +0100

    serialtest.py: integrate music

--- host-software/serialtest.py
+++ host-software/serialtest.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import sys, time
-import hwcontrol
+import hwcontrol, music
 
 if len(sys.argv) < 4:
     print(f'Usage: {sys.argv[0]} serialport valveTime count')
@@ -10,29 +10,45 @@ count = int(sys.argv[3])
 assert(valveTime >= 0)
 assert(count > 0)
 
+musicThread = music.MusicThread(name='music')
 
-with hwcontrol.Serial(sys.argv[1]) as ser:
-    ## Wait for Arduino to be ready:
-    posNum = ser.waitForReady()
-    if count > posNum:
-        print()
-        print(f'ERROR: Only {posNum} positions available, not the {count} that you ordered.')
-        sys.exit(1)
+def main():
+    ## Start music:
+    musicThread.start()
 
-    ## Home:
-    ser.home()
+    with hwcontrol.Serial(sys.argv[1]) as ser:
+        ## Wait for Arduino to be ready:
+        posNum = ser.waitForReady()
+        if count > posNum:
+            print()
+            print(f'ERROR: Only {posNum} positions available, not the {count} that you ordered.')
+            sys.exit(1)
 
-    ## Move to all positions:
-    try:
-        ser.pump(True)
-        time.sleep(1)
-        ser.valve(valveTime)
-        for p in range(1, count):
-            ser.moveto(p)
+        ## Home:
+        ser.home()
+
+        ## Move to all positions:
+        try:
+            ser.pump(True)
+            time.sleep(1)
             ser.valve(valveTime)
-    finally:
-        ser.pump(False)
+            for p in range(1, count):
+                ser.moveto(p)
+                ser.valve(valveTime)
+        finally:
+            ser.pump(False)
+            musicThread.stop()
 
-    ## ... and back to zero:
-    if count != 1:
-        ser.moveto(0)
+        ## ... and back to zero:
+        if count != 1:
+            ser.moveto(0)
+
+if __name__ == '__main__':
+    try:
+        main()
+    finally:
+        musicThread.stop()
+
+        ## Wait for music to finish:
+        print('Waiting for music to finish ...')
+        musicThread.join()
