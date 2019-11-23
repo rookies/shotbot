2f455bd257cf23756372414fcb0241616c254f2e
Robert Knauer <robert@capsaicin-dev.de>
2019-11-24 00:28:06 +0100

    use logger inside hwcontrol.py

--- host-software/hwcontrol.py
+++ host-software/hwcontrol.py
@@ -1,38 +1,16 @@
-import sys, time, serial, enum
+import sys, time, serial, enum, logging
 
-# TODO: Remove calls to sys.exit
-# TODO: Use logger
 # TODO: Allow registering good / bad responses and add callbacks?
-
-class MessageType(enum.Enum):
-    STATE = 1
-    READY = 2
-    CMD = 3
-    MSG = 4
+# TODO: Move line classes into own file
 
 class Line(object):
-    @staticmethod
-    def parse(line):
-        lineParts = line.upper().split(' ')
-
-        if lineParts[0] == 'STATE':
-            return StateLine.parse(lineParts[1:])
-        elif lineParts[0] == 'READY':
-            return ReadyLine.parse(lineParts[1:])
-        elif lineParts[0] == 'CMD':
-            return CmdLine.parse(lineParts[1:])
-        elif lineParts[0] == 'MSG':
-            return MsgLine.parse(lineParts[1:])
-        else:
-            print('Invalid line type')
-            return None
+    pass
 
 class StateLine(Line):
     @staticmethod
     def parse(lineParts):
         if len(lineParts) != 2:
-            print('Invalid number of parts')
-            return None
+            raise ValueError('Invalid number of parts')
 
         return StateLine(*lineParts)
 
@@ -47,18 +25,15 @@ class ReadyLine(Line):
     @staticmethod
     def parse(lineParts):
         if len(lineParts) != 2:
-            print('Invalid number of parts')
-            return None
+            raise ValueError('Invalid number of parts')
 
         if lineParts[0] != 'POSITIONS':
-            print('Invalid first part')
-            return None
+            raise ValueError('Invalid first part')
 
         try:
             positions = int(lineParts[1])
         except ValueError:
-            print('Invalid second part')
-            return None
+            raise ValueError('Invalid second part')
 
         return ReadyLine(positions)
 
@@ -72,8 +47,7 @@ class CmdLine(Line):
     @staticmethod
     def parse(lineParts):
         if len(lineParts) == 0:
-            print('Invalid number of parts')
-            return None
+            raise ValueError('Invalid number of parts')
         msg = ' '.join(lineParts[1:])
 
         if lineParts[0] == 'OK':
@@ -81,8 +55,7 @@ class CmdLine(Line):
         elif lineParts[0] == 'ERROR':
             return CmdLine(False, msg)
         else:
-            print('Invalid first part')
-            return None
+            raise ValueError('Invalid first part')
 
     def __init__(self, ok, message):
         self.ok = ok
@@ -98,8 +71,7 @@ class MsgLine(Line):
     @staticmethod
     def parse(lineParts):
         if len(lineParts) < 2:
-            print('Invalid number of parts')
-            return None
+            raise ValueError('Invalid number of parts')
 
         msg = ' '.join(lineParts[1:])
         return MsgLine(lineParts[0], msg)
@@ -112,18 +84,22 @@ class MsgLine(Line):
         return 'MsgLine: (%s) %s' % (self.level, self.message)
 
 class ShotBot(object):
-    def __init__(self, port):
+    def __init__(self, port, debug=False):
         # Create serial port:
         self._serial = serial.Serial(port, 86400, timeout=1)
 
         # Create hardware state:
         self._state = {}
 
-    def _sendline(self, line):
-        # Log it:
-        print('>>> %s' % line)
+        # Create logger:
+        self._logger = logging.getLogger(__name__)
+        if debug:
+            self._logger.setLevel(logging.DEBUG)
+        else:
+            self._logger.setLevel(logging.INFO)
 
-        # Send it:
+    def _sendline(self, line):
+        self._logger.debug('Sending line: %s', line)
         self._serial.write(('%s\n' % line).encode('ascii'))
 
     # TODO: Add timeout
@@ -136,10 +112,27 @@ class ShotBot(object):
                 continue
 
             # Log it:
-            print('<<< %s' % line)
+            self._logger.debug('Received line: %s', line)
 
             # Parse it:
-            parsedLine = Line.parse(line)
+            parsedLine = None
+            try:
+                lineParts = line.upper().split(' ')
+
+                if lineParts[0] == 'STATE':
+                    parsedLine = StateLine.parse(lineParts[1:])
+                elif lineParts[0] == 'READY':
+                    parsedLine = ReadyLine.parse(lineParts[1:])
+                elif lineParts[0] == 'CMD':
+                    parsedLine = CmdLine.parse(lineParts[1:])
+                elif lineParts[0] == 'MSG':
+                    parsedLine = MsgLine.parse(lineParts[1:])
+                else:
+                    raise ValueError('Invalid line type')
+            except ValueError as err:
+                self._logger.error(err)
+
+            # Check it:
             if parsedLine:
                 break
 
@@ -149,10 +142,12 @@ class ShotBot(object):
         # If it's a state line, save it to our state:
         if isinstance(parsedLine, StateLine):
             self._state[parsedLine.key] = parsedLine.value
+            # TODO: Fancier state output
             print('New state:', self._state)
 
         # If it's a message line, print it to the console:
         if isinstance(parsedLine, MsgLine):
+            # TODO: Output via logger
             print(parsedLine)
 
         # Return it:
@@ -166,45 +161,45 @@ class ShotBot(object):
                 return True
 
     def ready(self):
-        print('[hwcontrol] Waiting for the Arduino to be ready ...')
+        self._logger.info('Waiting for the Arduino to be ready ...')
         while True:
             line = self._readline()
             if isinstance(line, ReadyLine):
-                print('[hwcontrol] Ready, %d positions.' % line.positions)
+                self._logger.info('Ready, %d positions', line.positions)
                 return line.positions
 
     # TODO: Handle errors
     def home(self):
-        print('[hwcontrol] Homing ...')
+        self._logger.info('Homing ...')
         self._sendline('HOME')
         self._waitForState('HOMED', 'TRUE')
-        print('[hwcontrol] Homed.')
+        self._logger.info('Homed')
 
     # TODO: Handle errors
     def goto(self, position):
-        print('[hwcontrol] Moving to position %d ...' % position)
+        self._logger.info('Moving to position %d ...', position)
         self._sendline('GOTO %d' % position)
         self._waitForState('POSITION', str(position))
-        print('[hwcontrol] Moved to position %d' % position)
+        self._logger.info('Moved to position %d', position)
 
     # TODO: Handle errors
     def valve(self, millis):
-        print(f'[hwcontrol] Opening valve for {millis}ms ...')
+        self._logger.info('Opening valve for %dms ...', millis)
         self._sendline('VALVE %d' % millis)
         if millis != 0:
             self._waitForState('VALVE', 'OPEN')
         self._waitForState('VALVE', 'CLOSED')
-        print('[hwcontrol] Opening valve done.')
+        self._logger.info('Opening valve done')
 
     # TODO: Handle errors
     def pump(self, enable):
         if enable:
-            print('[hwcontrol] Starting pump ...')
+            self._logger.info('Starting pump ...')
             self._sendline('PUMP 1')
             self._waitForState('PUMP', 'ON')
-            print('[hwcontrol] Started pump.')
+            self._logger.info('Started pump')
         else:
-            print('[hwcontrol] Stopping pump and releasing pressure ...')
+            self._logger.info('Stopping pump and releasing pressure ...')
             self._sendline('PUMP 0')
             self._waitForState('PUMP', 'OFF')
-            print('[hwcontrol] Stopped pump.')
+            self._logger.info('Stopped pump')
