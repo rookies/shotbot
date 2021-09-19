34b641ce433c456f48de49f3b7aadfb438206fe5
Robert Knauer <robert@capsaicin-dev.de>
2021-09-19 13:10:42 +0200

    Move pump and stepper into own classes

---
+++ arduino-software/Hardware.hh
@@ -0,0 +1,206 @@
+#pragma once
+#include <AccelStepper.h>
+#include "Config.hh"
+#include "State.hh"
+
+/* This is a singleton. */
+class Pump {
+  public:
+    static Pump& getInstance() {
+      static Pump instance;
+      return instance;
+    }
+
+    Pump(Pump const&) = delete;
+    void operator=(Pump const&) = delete;
+
+    void on(uint8_t i, long duration) {
+      if (i >= pumpsNum) return;
+
+      if (duration <= 0 || duration > pumpMaxTime) {
+        duration = pumpMaxTime;
+      }
+
+      digitalWrite(pinPumps[i], HIGH);
+      state_set_onoff(F("PUMP"), i, true, duration);
+
+      /* Schedule switching off the pump: */
+      m_on = true;
+      m_switchOffTime = millis() + duration;
+      m_currentlyActivePump = i;
+    }
+
+    void off() {
+      if (m_currentlyActivePump >= pumpsNum) return;
+
+      digitalWrite(pinPumps[m_currentlyActivePump], LOW);
+      state_set_onoff(F("PUMP"), m_currentlyActivePump, false);
+
+      /* Unschedule switching off the pump: */
+      m_on = false;
+    }
+
+    void run() {
+      /* Checks if the pump has to be switched off and if so, switches it off. */
+      if (m_on && millis() >= m_switchOffTime) {
+        off();
+      }
+    }
+
+    bool isOn() {
+      return m_on;
+    }
+  private:
+    Pump() {
+      for (uint8_t i=0; i < pumpsNum; i++) {
+        pinMode(pinPumps[i], OUTPUT);
+        m_currentlyActivePump = i;
+        off();
+      }
+      m_currentlyActivePump = -1;
+    }
+
+    uint8_t m_currentlyActivePump;
+    bool m_on;
+    unsigned long m_switchOffTime;
+};
+
+
+/* This is a singleton. */
+class Stepper {
+  public:
+    static Stepper& getInstance() {
+      static Stepper instance;
+      return instance;
+    }
+
+    Stepper(Stepper const&) = delete;
+    void operator=(Stepper const&) = delete;
+
+    void on() {
+      m_stepper.enableOutputs();
+      state_set_onoff(F("STEPPER"), true);
+    }
+
+    void off() {
+      m_stepper.disableOutputs();
+      state_set_onoff(F("STEPPER"), false);
+
+      /* Remember that we're not homed anymore: */
+      m_homed = false;
+      state_set(F("HOMED"), false);
+    }
+
+    void run() {
+      /* If endstop switch is pressed, stop instantly: */
+      if (digitalRead(pinEndstop) == LOW) {
+        m_stepper.stop();
+        if (!m_endstopErrorPrinted) {
+          Serial.println(F("MSG ERROR ENDSTOPUNEXPECTED"));
+          m_endstopErrorPrinted = true;
+        }
+        /* Suppress POSREACHED message: */
+        m_targetPositionReached = true;
+        return;
+      }
+
+      /* If endstop is released, print a message once: */
+      if (m_endstopErrorPrinted) {
+        Serial.println(F("MSG INFO ENDSTOPRELEASED"));
+        m_endstopErrorPrinted = false;
+      }
+
+      /* Run the stepper motor: */
+      m_stepper.run();
+
+      /* Print a message when we arrived at our target: */
+      if (!m_targetPositionReached && m_stepper.distanceToGo() == 0) {
+        state_set(F("POSITION"), m_targetPosition);
+        m_targetPositionReached = true;
+      }
+    }
+
+    void home() {
+      /* TODO: Make non-blocking? */
+      state_set(F("BLOCKED"), true);
+
+      /* Run backwards until the endstop switch is pressed: */
+      m_stepper.setSpeed(-homeStepsPerSecond);
+      while (digitalRead(pinEndstop) == HIGH) {
+        m_stepper.runSpeed();
+      }
+      m_stepper.stop();
+
+      /* Zero the position: */
+      m_stepper.setCurrentPosition(0);
+
+      /* Go to minimal position: */
+      m_stepper.runToNewPosition(positionMin);
+
+      /* Remember that we homed: */
+      m_homed = true;
+      state_set(F("HOMED"), true);
+
+      /* Remember the current position: */
+      m_targetPosition = 0;
+      state_set(F("POSITION"), 0);
+
+      /* Suppress POSREACHED message: */
+      m_targetPositionReached = true;
+
+      state_set(F("BLOCKED"), false);
+    }
+
+    void move(char *command) {
+      /* TODO: Remove command parsing code. */
+      /* Check if we're homed: */
+      if (!m_homed) {
+        Serial.println(F("CMD ERROR NOTHOMED"));
+        return;
+      }
+
+      /* Extract position: */
+      int position = atoi(strchr(command, ' ') + 1);
+      if (position < 0 || position >= positionsNum) {
+        Serial.println(F("CMD ERROR INVALIDPOSITION"));
+        return;
+      }
+
+      /* Acknowledge command: */
+      Serial.println(F("CMD OK"));
+
+      /* Set the new target: */
+      m_targetPosition = position;
+      m_stepper.moveTo(positionMin + position * stepsPerPosition);
+      state_set(F("TARGET"), position);
+
+      /* Set positionReached: */
+      m_targetPositionReached = false;
+    }
+
+    bool isHomed() {
+      return m_homed;
+    }
+
+    static const long stepsPerPosition = (positionMax - positionMin) / (positionsNum - 1);
+  private:
+    Stepper() : m_homed(false), m_stepper(AccelStepper(AccelStepper::DRIVER, pinStep, pinDirection)),
+                m_targetPosition(0), m_targetPositionReached(true), m_endstopErrorPrinted(false) {
+      m_stepper.setEnablePin(pinEnable);
+      m_stepper.setPinsInverted(false, false, true);
+      m_stepper.setMaxSpeed(maxStepsPerSecond);
+      m_stepper.setAcceleration(acceleration);
+      on();
+
+      state_set(F("HOMED"), false);
+      state_set(F("TARGET"), -1);
+      state_set(F("POSITION"), -1);
+      state_set(F("BLOCKED"), false);
+    }
+
+    AccelStepper m_stepper;
+    bool m_homed;
+    uint8_t m_targetPosition;
+    bool m_targetPositionReached;
+    bool m_endstopErrorPrinted;
+};
--- arduino-software/arduino-software.ino
+++ arduino-software/arduino-software.ino
@@ -1,18 +1,10 @@
-#include <AccelStepper.h>
 #include <Adafruit_NeoPixel.h>
 #include "Config.hh"
-#include "TaskScheduler.hh"
 #include "CommandReader.hh"
 #include "SelectorKnob.hh"
 #include "DebouncedSwitch.hh"
 #include "State.hh"
-
-const long stepsPerPosition = (positionMax - positionMin) / (positionsNum - 1);
-uint8_t targetPosition;
-uint8_t currentlyActivePump = -1;
-bool targetPositionReached = true;
-bool endstopErrorPrinted = false;
-bool homed = false;
+#include "Hardware.hh"
 
 
 void parseCommand(char *);
@@ -27,56 +19,12 @@ DebouncedSwitch startButton(pinStartButton, true, 100);
 
 
 /* Outputs: */
-AccelStepper stepper(AccelStepper::DRIVER, pinStep, pinDirection);
 Adafruit_NeoPixel countLEDs(countLEDsNum, pinCountLEDs, NEO_KHZ800 + NEO_GRB);
 Adafruit_NeoPixel pumpLEDs(pumpsNum, pinPumpLEDs, NEO_KHZ800 + NEO_GRB);
 
 
 /* Utilities: */
-TaskScheduler<1> taskScheduler;
 CommandReader<commandLengthMax> commandReader(parseCommand);
-
-
-/* Task IDs: */
-const size_t taskId_switchOffPump = 0;
-
-
-/* HW Control: */
-void pumpOn(uint8_t i, long duration) {
-  if (i >= pumpsNum) return;
-
-  if (duration <= 0 || duration > pumpMaxTime) {
-    duration = pumpMaxTime;
-  }
-
-  digitalWrite(pinPumps[i], HIGH);
-  state_set_onoff(F("PUMP"), i, true, duration);
-
-  /* Schedule switching off the pump after pumpMaxTime: */
-  currentlyActivePump = i;
-  taskScheduler.scheduleTask(taskId_switchOffPump, duration);
-}
-void pumpOff() {
-  if (currentlyActivePump >= pumpsNum) return;
-
-  digitalWrite(pinPumps[currentlyActivePump], LOW);
-  state_set_onoff(F("PUMP"), currentlyActivePump, false);
-
-  /* Unschedule switching off the pump after pumpMaxTime: */
-  taskScheduler.unscheduleTask(taskId_switchOffPump);
-}
-void stepperOn() {
-  stepper.enableOutputs();
-  state_set_onoff(F("STEPPER"), true);
-}
-void stepperOff() {
-  stepper.disableOutputs();
-  state_set_onoff(F("STEPPER"), false);
-
-  /* Remember that we're not homed anymore: */
-  homed = false;
-  state_set(F("HOMED"), false);
-}
 
 
 void setup() {
@@ -84,99 +32,22 @@ void setup() {
   Serial.begin(serialBaudrate);
 
   /* Setup stepper motor: */
-  stepper.setEnablePin(pinEnable);
-  stepper.setPinsInverted(false, false, true);
-  stepper.setMaxSpeed(maxStepsPerSecond);
-  stepper.setAcceleration(acceleration);
-  stepperOn();
+  Stepper::getInstance(); /* TODO: Prevent this from being optimized away? */
 
   /* Setup endstop switch: */
   pinMode(pinEndstop, INPUT);
 
   /* Setup pumps: */
-  for (uint8_t i=0; i < pumpsNum; i++) {
-    pinMode(pinPumps[i], OUTPUT);
-    currentlyActivePump = i;
-    pumpOff();
-  }
-  currentlyActivePump = -1;
+  Pump::getInstance(); /* TODO: Prevent this from being optimized away? */
 
   /* Setup LEDs: */
   countLEDs.begin();
   pumpLEDs.begin();
 
-  /* Setup tasks: */
-  taskScheduler.setTask(taskId_switchOffPump, pumpOff);
-
-  /* We're not homed yet: */
-  homed = false;
-  state_set(F("HOMED"), false);
-  state_set(F("TARGET"), -1);
-  state_set(F("POSITION"), -1);
-  state_set(F("BLOCKED"), false);
-
   /* Report that we're ready: */
   state_set(F("READY"), true);
   state_set(F("POSITIONS"), positionsNum);
   state_set(F("PUMPS"), pumpsNum);
-}
-
-
-void cmd_home() {
-  state_set(F("BLOCKED"), true);
-
-  /* Run backwards until the endstop switch is pressed: */
-  stepper.setSpeed(-homeStepsPerSecond);
-  while (digitalRead(pinEndstop) == HIGH) {
-    stepper.runSpeed();
-  }
-  stepper.stop();
-
-  /* Zero the position: */
-  stepper.setCurrentPosition(0);
-
-  /* Go to minimal position: */
-  stepper.runToNewPosition(positionMin);
-
-  /* Remember that we homed: */
-  homed = true;
-  state_set(F("HOMED"), true);
-
-  /* Remember the current position: */
-  targetPosition = 0;
-  state_set(F("POSITION"), 0);
-
-  /* Suppress POSREACHED message: */
-  targetPositionReached = true;
-
-  state_set(F("BLOCKED"), false);
-}
-
-
-void cmd_goto(char *command) {
-  /* Check if we're homed: */
-  if (!homed) {
-    Serial.println(F("CMD ERROR NOTHOMED"));
-    return;
-  }
-
-  /* Extract position: */
-  int position = atoi(strchr(command, ' ') + 1);
-  if (position < 0 || position >= positionsNum) {
-    Serial.println(F("CMD ERROR INVALIDPOSITION"));
-    return;
-  }
-
-  /* Acknowledge command: */
-  Serial.println(F("CMD OK"));
-
-  /* Set the new target: */
-  targetPosition = position;
-  stepper.moveTo(positionMin + position * stepsPerPosition);
-  state_set(F("TARGET"), position);
-
-  /* Set positionReached: */
-  targetPositionReached = false;
 }
 
 
@@ -193,9 +64,9 @@ void cmd_pump(char *command) {
 
   /* Switch pump: */
   if (val == 0) {
-    pumpOff();
+    Pump::getInstance().off();
   } else {
-    pumpOn(val, 0);
+    Pump::getInstance().on(val, 0);
   }
 }
 
@@ -204,10 +75,10 @@ void parseCommand(char *command) {
   if (strcmp(command, "HOME") == 0) {
     /* Go to home position. */
     Serial.println(F("CMD OK BLOCKING"));
-    cmd_home();
+    Stepper::getInstance().home();
   } else if (strstr(command, "GOTO ") == command) {
     /* Go to given position. */
-    cmd_goto(command);
+    Stepper::getInstance().move(command);
   } else if (strstr(command, "PUMP ") == command) {
     /* Start or stop the pump. */
     cmd_pump(command);
@@ -220,12 +91,15 @@ void parseCommand(char *command) {
 int lastButtonValue = LOW; /* TODO: Remove this. */
 
 void loop() {
-  /* Run scheduled tasks: */
-  taskScheduler.run();
+  /* Check if pump needs to be switched off: */
+  Pump::getInstance().run();
 
   /* Run LEDs: */
   countLEDs.show();
   pumpLEDs.show();
+
+  /* Run stepper: */
+  Stepper::getInstance().run();
 
   /* Check for button press / release: */
   int buttonValue = startButton.get();
@@ -234,7 +108,7 @@ void loop() {
     lastButtonValue = buttonValue;
 
     if (buttonValue == HIGH) {
-      cmd_home();
+      Stepper::getInstance().home();
       /* TODO */
     }
   }
@@ -251,33 +125,6 @@ void loop() {
     pumpLEDs.setPixelColor(pumpSelector.get(), pumpLEDsColor);
   }
 
-  /* If endstop switch is pressed, stop instantly: */
-  if (digitalRead(pinEndstop) == LOW) {
-    stepper.stop();
-    if (!endstopErrorPrinted) {
-      Serial.println(F("MSG ERROR ENDSTOPUNEXPECTED"));
-      endstopErrorPrinted = true;
-    }
-    /* Suppress POSREACHED message: */
-    targetPositionReached = true;
-    return;
-  }
-
-  /* If endstop is released, print a message once: */
-  if (endstopErrorPrinted) {
-    Serial.println(F("MSG INFO ENDSTOPRELEASED"));
-    endstopErrorPrinted = false;
-  }
-
   /* Parse new commands: */
   commandReader.run();
-
-  /* Print a message when we arrived at our target: */
-  if (!targetPositionReached && stepper.distanceToGo() == 0) {
-    state_set(F("POSITION"), targetPosition);
-    targetPositionReached = true;
-  }
-
-  /* Run the stepper motor: */
-  stepper.run();
 }
